9657b710127c7e25b1fedc3e1fe80cf7a1fbeec4
Adam Boult <adamdboult@gmail.com>
2021-01-22 17:17:43 +0000

    Fixed gulp error

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var gulp   = require('gulp');
 var jshint = require('gulp-jshint');
 var sass   = require('gulp-sass');
 var exec   = require('child_process').exec;
+var spawn   = require('child_process').spawn;
 var del    = require('del');
 
 // JS Hint
@@ -14,7 +15,7 @@ gulp.task('jshint', function(cb) {
     gulp.src([__dirname+'/server.js', __dirname+'/gulpfile.js', __dirname+'/config/**/*.js', __dirname+'/src/**/*.js'])
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'));
-    
+	
     cb();
     
 });
@@ -26,9 +27,9 @@ gulp.task('emptyDestFolders', function(cb) {
         __dirname+'/built',
         __dirname+'/public'
     ]);
-    
+
     cb();
-    
+
 });
 
 // Favicon
@@ -36,9 +37,9 @@ gulp.task('favicon', function(cb) {
 
     gulp.src(__dirname + '/src/img/compiled/*')
 	.pipe(gulp.dest(__dirname+'/public/'));
-
+	
     cb();
-    
+
 });
 
 // Packages
@@ -59,9 +60,9 @@ gulp.task('packages', function(cb) {
     // Popper
     gulp.src(__dirname+'/node_modules/popper.js/dist/umd/**/*')
       .pipe(gulp.dest(__dirname+'/public/packages/popper.js/'));
-
+      
     cb();
-    
+
 });
 
 // Scripts
@@ -69,7 +70,7 @@ gulp.task('scripts', function(cb) {
     
     gulp.src(__dirname+'/src/js/**/*')
       .pipe(gulp.dest(__dirname+'/public/js/'));
-    
+      
     cb();
     
 });
@@ -83,27 +84,25 @@ gulp.task('styles', function(cb) {
        
     gulp.src(__dirname+'/src/styles/**/*.css')
        .pipe(gulp.dest(__dirname+'/public/css/'));
-
+       
     cb();
-    
+
 });
 
 // Latex
-gulp.task('latex', function(cb) {
+gulp.task('latex', function() {
 
-    var latexSrc = __dirname + '/src/pug/**/*',
-        latexDst = __dirname + '/built/pug';
+    var latexSrc = __dirname + '/src/pug/**/*';
+    var latexDst = __dirname + '/built/pug';
 
-    gulp.src(latexSrc)
+    return gulp.src(latexSrc)
         .pipe(gulp.dest(latexDst));
-        
-    cb();
 
 });
 
 // Latex convert
 gulp.task('latexConvert', function(cb) {
-
+    
     exec('./convert_latex.sh', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
