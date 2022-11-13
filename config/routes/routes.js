250d79ecb9fc222f0b9c3333c1f9aadcd9d9971d
Adam Boult <adamdboult@gmail.com>
2022-11-13 21:11:10 +0000

    Minor changes.

--- config/routes/routes.js
+++ config/routes/routes.js
@@ -4,48 +4,6 @@ var fs=require('fs');
 //var parseString = require('xml2js').parseString;
 var modelArray=[];
 var blogArray=[];
-var rssString='<?xml version="1.0" encoding="UTF-8" ?>';
-rssString   +='<rss version ="2.0">';
-rssString   +='<channel>';
-rssString   +='<title>YetiPredict</title>';
-rssString   +='<link>http://www.yetipredict.com</link>';
-rssString   +='<description>RSS feed for YetiPredict</description>';
-
-rssString   +='<image>';
-rssString   +='<url>http://www.yetipredict.com/img/yetiPredict.ico</url>';
-rssString   +='<link>http://www.yetipredict.com</link>';
-rssString   +='</image>';
-var postTitle;
-var postDate;
-var postNoType;
-var postFull;
-
-fs.readdir(__dirname+'/../../src/pug/blog', function(err, files){
-    files.forEach(function(f){
-	postTitle=f.substring(8,f.length-4);
-	postDate=new Date(f.substring(0,4),f.substring(4,6)-1,f.substring(6,8));
-	postNoType=f.substring(0,f.length-4);
-	postFull=f;
-	blogArray.push({
-	    full:postFull,
-	    noType:postNoType,
-	    //month -1 as starts from 0
-	    date:postDate,
-	    title:postTitle
-	});
-	rssString +='<item>';
-	rssString +='<title>'+postTitle+'</title>';
-	rssString +='<link>http://www.yetipredict.com/news/'+postNoType+'</link>';
-	rssString +='<description>'+postTitle+'</description>';
-	rssString +='<pubDate>'+postDate+'</pubDate>';
-	rssString +='<author>Adam Boult</author>';
-	rssString +='</item>';
-	
-    });
-
-    rssString +='</channel>';
-    rssString +='</rss>';
-});
 
 module.exports=function(app){
     'use strict';
@@ -67,11 +25,6 @@ module.exports=function(app){
     });*/
     app.get('/theory', function(req,res){
 	res.redirect('/');
-    });
-
-    app.get('/rss', function(req,res){
-	res.set('Content-Type', 'application/rss+xml');
-	res.send(rssString);
     });
 
     app.get('/about', function(req,res){
