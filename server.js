d02a4bd29f26c7ff99a562ae9247ab2b28c6ed80
Adam Boult <adamdboult@gmail.com>
2024-07-20 11:09:26 +0100

    More

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ var app = express();
 //ROUTES
 app.use(express.static(__dirname + '/public'));// set the static files location /public/img will be /img for users
 
-//app.locals.pretty=true;
+//pretty makes the html not just 1 line, and so is readable
+app.locals.pretty=true;
 app.set('views',__dirname+'/built/pug/');
 app.set('view engine', 'pug');
 
